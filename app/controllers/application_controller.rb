40fc60b1bdc422272de5a3e6e7ee581eab1c8082
lampv <lampvhy@gmail.com>
2019-12-05 16:53:40 +0900

    require login admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
+  # before_action :authenticate_admin_user!
+
+  def authenticate_admin_user!
+    if current_user.nil?
+      redirect_to login_url
+    end
+  end
 end
