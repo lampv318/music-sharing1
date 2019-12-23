f44d9f240c815f94ece70d50c0f4d54ccac4517d
lampv <lampvhy@gmail.com>
2019-12-23 13:41:36 +0900

    update song api test

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,7 +3,7 @@ module Api
 
     def present_or_not_found object
       return if object.present?
-      render body: nil, status: :not_found
+      render json: { error: "not found", status: 404 }
     end
   end
 end
